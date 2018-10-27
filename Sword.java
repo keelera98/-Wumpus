d81d29b425cd3406df953db6b04fd30a2cdabee3
keelera98 <32946655+keelera98@users.noreply.github.com>
2018-10-27 16:48:23 -0400

    Delete Sword.java

--- Sword.java
+++
@@ -1,9 +0,0 @@
-public class Sword extends Weapon
-{
-
-    public Sword(Handler handler, int x, int y, int width, int height)
-    {
-        super(handler, x, y, width, height, Weapon.swordDamage, Weapon.swordID);
-
-    }
-}
