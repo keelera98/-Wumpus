b603ddd8e14646eaab82f5bfd61e9b3ee084a61b
keelera98 <32946655+keelera98@users.noreply.github.com>
2018-10-27 16:46:19 -0400

    Delete BowArrow.java

--- BowArrow.java
+++
@@ -1,8 +0,0 @@
-public class BowArrow extends Weapon
-{
-    public BowArrow(Handler handler, int x, int y, int width, int height)
-    {
-        super(handler, x, y, width, height, Weapon.bowDamage, Weapon.slayerID);
-
-    }
-}
