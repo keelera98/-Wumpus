87888fb2d3bb7f43eaa384f183ce96a969c4ab3f
ClarkRossi <34036666+ClarkRossi@users.noreply.github.com>
2018-10-27 21:58:50 -0400

    Add files via upload

--- NotWumpus/src/Launcher.java
+++ NotWumpus/src/Launcher.java
@@ -1,6 +1,6 @@
 public class Launcher {
     public static void main(String args[]){
-        Game game = new Game("Not Wumpus", 900, 832);
+        Game game = new Game("Not Wumpus", 1200, 832);
         game.start();
     }
 }
